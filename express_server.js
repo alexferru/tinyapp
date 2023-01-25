bee978f97c572b579226a4960c76acc7d4d69756
JimHwkins <howlarrior@gmail.com>
2023-01-24 21:53:55 -0300

    Added Login button and its POST route

--- express_server.js
+++ express_server.js
@@ -59,6 +59,11 @@ app.post("/urls/:id/edit", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("urls/");
+});
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
