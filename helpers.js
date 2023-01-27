4e12314da13adfc136aa30b0cc332f9c65034beb
JimHwkins <howlarrior@gmail.com>
2023-01-26 23:52:59 -0300

    Added Docs dir and some minor changes to views

--- helpers.js
+++ helpers.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
 
 //------ HELPERS ------
+
+// The function returns a random generated string, it takes a Number as a parameter
+// that defines the length of the generated string
 const generateRandomString = (length) => {
   let result = "";
   const characters =
@@ -12,6 +15,9 @@ const generateRandomString = (length) => {
   return result;
 };
 
+// The function looks for the email key inside the database object and returns
+// the user object that matches the email key,
+// if the key doesn't match, it returns false
 const getUserByEmail = (email, database) => {
   for (let userID in database) {
     const user = database[userID];
@@ -22,6 +28,8 @@ const getUserByEmail = (email, database) => {
   return false;
 };
 
+//The function returns the urls that match the id key
+// it returns an empty object if the id doesn't match
 const urlsForUser = (database, id) => {
   let newObj = {};
   for (let key in database) {
@@ -34,6 +42,8 @@ const urlsForUser = (database, id) => {
   return newObj;
 };
 
+// The function creates a new user inside the database object and return its random generated ID
+// it takes an email, a password and a database as parameters
 const createUser = (email, password, database) => {
   const userID = generateRandomString(6);
   database[userID] = {
@@ -44,6 +54,8 @@ const createUser = (email, password, database) => {
   return userID;
 };
 
+//The function returns a user object that matches the parameters
+// if the user is not found returns false
 const authenticateUser = (email, password, database) => {
   const userFound = getUserByEmail(email, database);
   if (userFound && bcrypt.compareSync(password, userFound.password)) {
